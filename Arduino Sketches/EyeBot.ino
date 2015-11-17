7d8cabcedd7f12ad0d9112bf7dd0cb9dd07cf345
crboth <crboth@tamu.edu>
2015-11-17 15:32:12 -0600

    Update EyeBot.ino

--- Arduino Sketches/EyeBot.ino
+++ Arduino Sketches/EyeBot.ino
@@ -1,6 +1,6 @@
-#include <Servo.h>
-
-enum direction { FORWARD, BACKWARD };
+#include <Servo.h>          // import the servo library
+#include <math.h>           // import the math library
+#include <SoftwareSerial.h> // import the serial library
 
 Servo platform;
 Servo shoulder;
@@ -20,14 +20,16 @@ Servo claw;
 /** LIMITATIONS **/
 #define platformMax 2100
 #define platformMin 800
-#define shoulderMax 1900
-#define shoulderMin 1100
-#define elbowMax 2100
-#define elbowMin 1300
-#define wristMax 1900
-#define wristMin 1100
-#define clawMax 1900
-#define clawMin 1100
+#define shoulderMax 2300
+#define shoulderMin 700
+#define elbowMax 2400
+#define elbowMin 600
+#define wristPitchMax 1900
+#define wristPitchMin 1100
+#define wristRotationMax 1900
+#define wristRotationMin 1100
+#define clawMax 2250
+#define clawMin 550
 
 /*** SERVO LOCATIONS ***/
 int platformLocation;
@@ -51,27 +53,25 @@ int clawLocation;
  * 
  */
 
+//SoftwareSerial Bluetooth(2, 3);  // RX, TX for bluetooth
 int Byte1Received;
 int Byte2Received;
 char val;         // variable to receive data from the serial port
 byte data = 0; //value to transmit, binary 10101010
 byte mask = 1; //our bitmask
-direction movement_direction = FORWARD; //default movement direction
 
 /****** SETUP: RUNS ONCE ******/
 void setup()   
 {
+  
   Serial.begin(9600);  
   Serial.println("--- EyeBot Arduino is set up ---");
-  Serial.println(" Please input a command in the form [servo]:[action] ");
-  Serial.println("\n\tServos: pl, sh, el, wr, cl");  
-  Serial.println("\tActions: right, left, up, down, open, close"); 
 
   platformLocation = 1500;
-  shoulderLocation = 1700;
-  elbowLocation = 1800;
+  shoulderLocation = 1500;
+  elbowLocation = 2050;
   wristPitchLocation = 1500;
-  wristRotationLocation = 1500;
+  wristRotationLocation = 1650;
   clawLocation = 1500;
 
   platform.attach(platformPort);
@@ -100,37 +100,38 @@ void loop()
 {
   if (Serial.available() > 0)
   {
-      data = Serial.read();         // read it and store it in 'val'
-      val = data;
-      Serial.print("Value Received: ");
-      Serial.print(data, BIN);
-      Serial.print(" ");
-      Serial.println(val);
+    data = Serial.read();         // read it and store it in 'val'
+    val = data;
+    Serial.print("Value Received: ");
+    Serial.print(data, BIN);
+    Serial.print(" ");
+    Serial.println(val);
 
-      bool command[8];
-      mask = 00000001;
-      for (int i = 0; i < 8; ++i) {
+    bool command[8];
+    mask = 00000001;
+    for (int i = 0; i < 8; ++i) {
         command[i] = data & mask;
         mask <<= 1;
-      }
+    }
 
-    val = data;
+    byte command_standard = data & 64;
+    byte command_direction = data & 32;
+    byte command_type = data & 24;
+    byte command_number = data & 7;
+
     int movement = 0;
-    movement_direction = direction(command[5]);
-    if (movement_direction == FORWARD) {
-      Serial.println("FORWARD");
+    if (command_direction == 32) {
+      Serial.println("FORWARD");     //lowercase
       movement = servoSpeed;
     } else {
-      Serial.println("BACKWARD");
+      Serial.println("BACKWARD");    //uppercase
       movement = -servoSpeed;
     }
     
-    if (!command[4] && !command[3]) {  //simple servo movements
-      Serial.println("simple");
 
-      byte command = data & 7;
-
-      switch(command) {
+   if (command_type == 0) {
+    
+      switch(command_number) {
 
         case 0:   //nothing
           break;
@@ -162,26 +163,77 @@ void loop()
           break;
       }
 
-      platform.writeMicroseconds(platformLocation);
-      shoulder.writeMicroseconds(shoulderLocation);
-      elbow.writeMicroseconds(elbowLocation);
-      wrist_pitch.writeMicroseconds(wristPitchLocation);
-      wrist_rotation.writeMicroseconds(wristRotationLocation);
-      claw.writeMicroseconds(clawLocation);
-      
     }
-    else if (!command[4] && command[3]) {  //complex movements
+    else if (command_type == 8) {  //complex movements
       Serial.println("complex");
 
+      double shoulder_rad = (shoulderLocation - shoulderMin) / (shoulderMax-shoulderMin) * PI;
+      double elbow_rad = PI - (elbowLocation - elbowMin) / (elbowMax-elbowMin) * PI;
+      double elbow_relative = elbow_rad + shoulder_rad - PI;
+      double wrist_rad = (wristPitchLocation - wristPitchMin) / (wristPitchMax-wristPitchMin) * PI;
+      double shoulder_inc = .03;
+      switch(command_number) {
+
+        case 0:   //nothing
+          break;
+        case 1:   //platform
+        {
+          Serial.println("Forward / Backward");
+
+
+          //double asin_value = asin();
+          
+          double L1 = 9;
+          double L2 = 5;
+          elbow_relative += asin((L1*sin(shoulder_rad)+L2*sin(elbow_relative) - L1*sin(shoulder_rad+shoulder_inc))/L2);
+          shoulder_rad += shoulder_inc;
+          elbow_rad = elbow_relative - shoulder_rad + PI;
+         
+          break;
+        }
+        case 2:
+        {
+          Serial.println("Up / Down");
+          break;
+        }
+        case 3:
+          Serial.println("Follow Claw Forward / Backward");
+          break;
+        default:
+          break;
+      }
+
+
+
+      shoulderLocation  = ((shoulder_rad * (shoulderMax - shoulderMin) / PI) + shoulderMin);
+      elbowLocation     = (((PI - elbow_rad) * (elbowMax - elbowMin) / PI) + elbowMin);
+//      wrist_pitch.writeMicroseconds((wrist_rad * (wristPitchMax - wristPitchMin) / PI) + wristPitchMin);
 
       
     }
-    else if (command[4]) {
+    else if (command_type == 16 || command_type == 24) {
       Serial.println("roomba");
-
       
     }
+
+    platform.writeMicroseconds(platformLocation);
+    Serial.print("Platform: ");
+    Serial.println(platformLocation);
+    shoulder.writeMicroseconds(shoulderLocation);
+    Serial.print("Shoulder: ");
+    Serial.println(shoulderLocation);
+    elbow.writeMicroseconds(elbowLocation);
+    Serial.print("Elbow: ");
+    Serial.println(elbowLocation);
+    wrist_pitch.writeMicroseconds(wristPitchLocation);
+    Serial.print("Wrist Pitch: ");
+    Serial.println(wristPitchLocation);
+    wrist_rotation.writeMicroseconds(wristRotationLocation);
+    Serial.print("Wrist Rotation: ");
+    Serial.println(wristRotationLocation);
+    claw.writeMicroseconds(clawLocation);
+    Serial.print("Claw: ");
+    Serial.println(clawLocation);
   }
 }
-
 
